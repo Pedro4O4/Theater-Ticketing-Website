e8be9ba4b653c867a59905541997dafcd20dc1b7
Pedro4O4 <ebram.attia@student.giu-uni.de>
2025-04-16 13:13:12 +0200

    handling

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,11 +1,10 @@
 
 const express = require('express');
-const { register, login, authenticate } = require('../Middleware/authenticationMiddleware');
 const authorize = require('../Middleware/authorizationMiddleware');
 const router = express.Router();
+const userController = require('../controllers/UserController');
+const {authenticate, authorizeRole} = require('../middleware/authenticationMiddleware');
 
-router.post('/register', register);
-router.post('/login', login);
 
 // Protected routes
 router.get('/admin', authenticate, authorize('Admin'), (req, res) => {
@@ -21,19 +20,13 @@ router.get('/user', authenticate, authorize('User'), (req, res) => {
 });
 
 
-
-        const express = require('express');
-        const router = express.Router();
-        const userController = require('../controllers/UserController');
-        const {authenticate, authorizeRole} = require('../middleware/authMiddleware');
-
         router.get('/profile', authenticate, userController.getUserProfile);
         router.put('/profile', authenticate, userController.updateUserProfile);
 
-        router.delete('/:id', authenticate, authorizeRole('System Admin'), userController.deleteUser);
-        router.get('/bookings', authenticate, authorizeRole('Standard User'), userController.getUserBookings);
-        router.get('/events', authenticate, authorizeRole('Organizer'), userController.getOrganizerEvents);
-        router.get('/events/analytics', authenticate, authorizeRole('Organizer'), userController.getEventAnalytics);
+        router.delete('/:id', authenticate, authorizeRole('System Admin'), userController.DeleteUser);
+        router.get('/bookings', authenticate, authorizeRole('Standard User'), userController.GetUserBookings);
+        router.get('/events', authenticate, authorizeRole('Organizer'), userController.GetOrganizerEvents);
+        router.get('/events/analytics', authenticate, authorizeRole('Organizer'), userController.GetOrganizerAnalytics);
 r
         module.exports = router;
 
