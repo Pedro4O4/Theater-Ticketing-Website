2da35e598f8a660cfbb26779ce96be06f7f5caa5
abdelmonem <abdelmonem.somida@student.giu-uni.de>
2025-04-19 15:00:59 +0200

    handling some wrong data

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -2,25 +2,24 @@ const express = require('express');
 const UserController = require('../Controllers/UserController');
 const authenticationMiddleware = require('../Middleware/authenticationMiddleware');
 const authorizationMiddleware = require('../Middleware/authorizationMiddleware');
-
 const router = express.Router();
 
 // Apply authentication middleware to all routes
 router.use(authenticationMiddleware);
 
 // Admin-only routes
-router.get('/', authorizationMiddleware('admin'), UserController.getAllUsers);
-router.get('/:id', authorizationMiddleware('admin'), UserController.getUserById);
-router.put('/:id', authorizationMiddleware('admin'), UserController.updateUserRole);
+router.get('/', authorizationMiddleware(['System Admin']), UserController.getAllUsers);
+router.get('/:id', authorizationMiddleware(['System Admin']), UserController.getUserById);
+router.put('/:id', authorizationMiddleware(['System Admin']), UserController.updateUserRole);
 
-module.exports = router;
+
 
 // Protected routes
-router.get('/admin',  authorizationMiddleware('Admin'), (req, res) => {
+router.get('/admin',  authorizationMiddleware(['System Admin']), (req, res) => {
     res.status(200).json({ message: "Welcome Admin" });
 });
 
-router.get('/organizer', authorizationMiddleware('Organizer'), (req, res) => {
+router.get('/organizer', authorizationMiddleware('organiser'), (req, res) => {
     res.status(200).json({ message: "Welcome Organizer" });
 });
 
@@ -32,10 +31,10 @@ router.get('/user',  authorizationMiddleware('User'), (req, res) => {
         router.get('/profile',  UserController.getUserProfile);
         router.put('/profile',  UserController.updateUserProfile);
 
-        router.delete('/:id', authorizationMiddleware('System Admin'), UserController.DeleteUser);
-        router.get('/bookings',  authorizationMiddleware('Standard User'), UserController.GetUserBookings);
-        router.get('/events',  authorizationMiddleware('Organizer'), UserController.GetOrganizerEvents);
-        router.get('/events/analytics', authorizationMiddleware('Organizer'), UserController.GetOrganizerAnalytics);
+        router.delete('/:id', authorizationMiddleware(['System Admin']), UserController.DeleteUser);
+        router.get('/bookings',  authorizationMiddleware(['Standard User']), UserController.GetUserBookings);
+        router.get('/events',  authorizationMiddleware(['Organizer']), UserController.GetOrganizerEvents);
+        router.get('/events/analytics', authorizationMiddleware(['Organizer']), UserController.GetOrganizerAnalytics);
 
 
 
