5975c702f1c2c143a80f3e7bc710fbded2380e8f
Pedro4O4 <ebram.attia@student.giu-uni.de>
2025-05-21 22:46:23 +0300

    Handle css of LoginForm.css

--- frontend/FrontendTheater/src/components/LoginForm.jsx
+++ frontend/FrontendTheater/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
+import './LoginForm.css'
 
 export default function LoginForm() {
     const [formData, setFormData] = useState({
