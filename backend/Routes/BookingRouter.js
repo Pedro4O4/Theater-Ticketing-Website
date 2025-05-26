4ae2fe69a47f6491c0963650576195512f3c35dc
Pedro4O4 <ebram.attia@student.giu-uni.de>
2025-05-26 12:00:56 +0300

    handling

--- backend/Routes/BookingRouter.js
+++ backend/Routes/BookingRouter.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const BookingController = require("../Controllers/BookingController");
-const authorizationMiddleware=require('../middleware/authorizationMiddleware')
- const authenticationMiddleware = require("../Middleware/authenticationMiddleware");
+const authorizationMiddleware = require('../Middleware/authorizationMiddleware');
+const authenticationMiddleware = require("../Middleware/authenticationMiddleware");
 const router = express.Router();
 
-// Middleware to check if the user is authenticated
-router.use(authenticationMiddleware);
 // Route to create a new booking
-router.post("/",authorizationMiddleware('Standard User'), BookingController.createBooking);
-router.get("/:id",authorizationMiddleware('Standard User'), BookingController.getBooking);
-router.delete("/:id",authorizationMiddleware('Standard User'), BookingController.deleteBooking);
-// Route to get all bookings
+router.post("/", authorizationMiddleware('Standard User'), BookingController.createBooking);
+router.get("/:id", authorizationMiddleware('Standard User'), BookingController.getBooking);
+router.delete("/:id", authorizationMiddleware('Standard User'), BookingController.deleteBooking);
+
 module.exports = router;
