29a2d82bb497fadc3b0a542b23943c170ce74ebf
Pedro4O4 <ebram.attia@student.giu-uni.de>
2025-05-26 18:22:08 +0300

    mr7ban bkom

--- frontend/FrontendTheater/vite.config.js
+++ frontend/FrontendTheater/vite.config.js
@@ -1,21 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default ({ mode }) => {
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd());
-
-  return defineConfig({
-    plugins: [react()],
-    base: env.VITE_BASE_PATH || '/', // Try with root path if unsure
-    define: {
-      // Make env variables available to the client code
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL)
-    },
-    css: {
-      preprocessorOptions: {
-        css: { charset: false }
-      }
-    }
-  });
-};
+export default defineConfig({
+  plugins: [react()],
+});
