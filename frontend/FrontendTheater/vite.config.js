4a9d6f4dc8cf7a6a8797396a398b6d4acb594d99
Pedro4O4 <ebram.attia@student.giu-uni.de>
2025-05-26 04:48:18 +0300

    deploy3

--- frontend/FrontendTheater/vite.config.js
+++ frontend/FrontendTheater/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // eslint-disable-next-line no-undef
-  base: process.env.VITE_BASE_PATH || '/masr7/frontend/FrontendTheater',
+  base: process.env.VITE_BASE_PATH || '/masr7/',
   css: {
     preprocessorOptions: {
       css: { charset: false }
