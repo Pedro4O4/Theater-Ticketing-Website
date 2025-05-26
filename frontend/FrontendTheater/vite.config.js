3d66328ce69fe1b4967625972b8a6bbf333df901
Pedro4O4 <ebram.attia@student.giu-uni.de>
2025-05-26 13:14:06 +0300

    handling

--- frontend/FrontendTheater/vite.config.js
+++ frontend/FrontendTheater/vite.config.js
@@ -7,7 +7,7 @@ export default ({ mode }) => {
 
   return defineConfig({
     plugins: [react()],
-    baseURL: import.meta.env.VITE_BACKEND_URL + 'api/v1',
+    base: env.VITE_BASE_PATH || '/', // Try with root path if unsure
     define: {
       // Make env variables available to the client code
       'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL)
